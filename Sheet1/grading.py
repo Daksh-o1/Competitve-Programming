aebbfa3801cd1441ef3d7578144d3dbff15a039b
Daksh-o1 <dakshverma7898@gmail.com>
2025-08-24 18:14:13 +0530

    Update grading.py

--- Sheet1/grading.py
+++ Sheet1/grading.py
@@ -1,3 +1,18 @@
-n = int(input("Enter a number: "))
-for i in range(1, n+1):
-    print(i, end=' ')
+try:
+    percentage = float(input("Enter your percentage: "))
+    if percentage > 100:
+        print("Percentage cannot be greater than 100.")
+    elif percentage >= 90:
+        print("Grade: A")
+    elif percentage >= 80:
+        print("Grade: B")
+    elif percentage >= 70:
+        print("Grade: C")
+    elif percentage >= 60:
+        print("Grade: D")
+    elif percentage >=50:
+        print("Grade: E")
+    else:
+        print("Grade: F")
+except ValueError:
+    print("Please enter a valid number.")
