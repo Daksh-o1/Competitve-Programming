f23430361affc2b8c5bd51adbcae6bbd0382fd5c
Daksh-o1 <dakshverma7898@gmail.com>
2025-08-24 18:15:04 +0530

    Update maximumOfTwoNum.py

--- Sheet1/maximumOfTwoNum.py
+++ Sheet1/maximumOfTwoNum.py
@@ -1,4 +1,4 @@
-n = int(input("Enter a number: "))
-while n> 0:
-    print(n, end=' ')
-    n-=1
+num1 = int(input("enter 1st num: "))
+num2 = int(input("enter 2nd num: "))
+if num1 > num2:
+    print(num1, "is greater than", num2)
