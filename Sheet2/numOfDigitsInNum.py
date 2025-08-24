67b458e8a285057c9dada644534f51e7881f8027
Daksh-o1 <dakshverma7898@gmail.com>
2025-08-24 18:22:27 +0530

    Update numOfDigitsInNum.py

--- Sheet2/numOfDigitsInNum.py
+++ Sheet2/numOfDigitsInNum.py
@@ -1,6 +1,7 @@
-n = int(input("Enter a number: "))
-sum = 0
-for i in range(1, n+1):
-    sum+=i
+num = int(input("Enter a number: "))
+count = 0 
+while num > 0:
+    num //= 10
+    count += 1
 
-print(f"Sum of first {n} numbers is: {sum}")
+print("Number of digits in the number:", count)
