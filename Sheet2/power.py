a09fbc0f185050d3af07aba3b22b07ddfef2d517
Daksh-o1 <dakshverma7898@gmail.com>
2025-08-24 18:25:14 +0530

    Update power.py

--- Sheet2/power.py
+++ Sheet2/power.py
@@ -2,3 +2,13 @@ A = int(input("Enter the base (A): "))
 B = int(input("Enter the exponent (B): "))
 result = A ** B
 print("The value of", A, "^", B, "is", result)
+
+#------------------------ USING FOR LOOP -------------------------
+
+num = int(input("Enter number: "))
+power = int(input("Enter the power: "))
+output = num
+for i in range(1, power):
+    # print(num)
+    output = output * num
+print(f"{num} power {power} is: {output}")
